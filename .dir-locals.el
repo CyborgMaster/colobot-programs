e0b66a2fa69b2cdb1b73e23f5f9ee0122242b37d
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2016-07-26 19:23:24 -0600

    Updated constants to new format added dir local variables

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+         (mode . c++)
+         (indent-tabs-mode . t)
+         (tab-width . 2)
+         )))
+;;  mode: c++; indent-tabs-mode: t; tab-width: 4; local-preserve-whitespace: t;
